964ca9e171ded53c19a319ba67e329681d2297a9
Kelly Selden <kellyselden@gmail.com>
2017-10-07 16:26:26 -0700

    refactor test reuse

--- test/index-test.js
+++ test/index-test.js
@@ -8,7 +8,12 @@ function clone(obj) {
 }
 
 describe('Unit - applyPatch', function() {
-  function test(myPackageJson, patch, toPackageJson, expected) {
+  function test(options) {
+    let myPackageJson = options.myPackageJson;
+    let patch = options.patch;
+    let toPackageJson = options.toPackageJson;
+    let expected = options.expected;
+
     let patchClone = clone(patch);
 
     applyPatch(myPackageJson, patch, toPackageJson);
@@ -19,168 +24,168 @@ describe('Unit - applyPatch', function() {
   }
 
   it('works in the beginning', function() {
-    let myPackageJson = {
-      test6: 1,
-      test1: 1,
-      test2: 1
-    };
-    let patch = [
-      { op: 'add', path: '/test4', value: 1 }
-    ];
-    let toPackageJson = {
-      test3: 1,
-      test4: 1,
-      test5: 1,
-      test1: 1,
-      test2: 1
-    };
-    let expected = {
-      test6: 1,
-      test4: 1,
-      test1: 1,
-      test2: 1
-    };
-
-    test(myPackageJson, patch, toPackageJson, expected);
+    test({
+      myPackageJson: {
+        test6: 1,
+        test1: 1,
+        test2: 1
+      },
+      patch: [
+        { op: 'add', path: '/test4', value: 1 }
+      ],
+      toPackageJson: {
+        test3: 1,
+        test4: 1,
+        test5: 1,
+        test1: 1,
+        test2: 1
+      },
+      expected: {
+        test6: 1,
+        test4: 1,
+        test1: 1,
+        test2: 1
+      }
+    });
   });
 
   it('works in the middle', function() {
-    let myPackageJson = {
-      test1: 1,
-      test6: 1,
-      test2: 1
-    };
-    let patch = [
-      { op: 'add', path: '/test4', value: 1 }
-    ];
-    let toPackageJson = {
-      test1: 1,
-      test3: 1,
-      test4: 1,
-      test5: 1,
-      test2: 1
-    };
-    let expected = {
-      test1: 1,
-      test6: 1,
-      test4: 1,
-      test2: 1
-    };
-
-    test(myPackageJson, patch, toPackageJson, expected);
-  });
-
-  it('works at the end', function() {
-    let myPackageJson = {
-      test1: 1,
-      test2: 1,
-      test6: 1
-    };
-    let patch = [
-      { op: 'add', path: '/test4', value: 1 }
-    ];
-    let toPackageJson = {
-      test1: 1,
-      test2: 1,
-      test3: 1,
-      test4: 1,
-      test5: 1
-    };
-    let expected = {
-      test1: 1,
-      test2: 1,
-      test6: 1,
-      test4: 1
-    };
-
-    test(myPackageJson, patch, toPackageJson, expected);
-  });
-
-  it('handles multiple patch', function() {
-    let myPackageJson = {
-      test1: 1,
-      test6: 1,
-      test2: 1,
-      test10: 1,
-      test0: 1
-    };
-    let patch = [
-      { op: 'add', path: '/test4', value: 1 },
-      { op: 'add', path: '/test8', value: 1 }
-    ];
-    let toPackageJson = {
-      test1: 1,
-      test3: 1,
-      test4: 1,
-      test5: 1,
-      test2: 1,
-      test7: 1,
-      test8: 1,
-      test9: 1,
-      test0: 1
-    };
-    let expected = {
-      test1: 1,
-      test6: 1,
-      test4: 1,
-      test2: 1,
-      test10: 1,
-      test8: 1,
-      test0: 1
-    };
-
-    test(myPackageJson, patch, toPackageJson, expected);
-  });
-
-  it('handles nested objects', function() {
-    let myPackageJson = {
-      test: {
+    test({
+      myPackageJson: {
         test1: 1,
         test6: 1,
         test2: 1
-      }
-    };
-    let patch = [
-      { op: 'add', path: '/test/test4', value: 1 }
-    ];
-    let toPackageJson = {
-      test: {
+      },
+      patch: [
+        { op: 'add', path: '/test4', value: 1 }
+      ],
+      toPackageJson: {
         test1: 1,
         test3: 1,
         test4: 1,
         test5: 1,
         test2: 1
-      }
-    };
-    let expected = {
-      test: {
+      },
+      expected: {
         test1: 1,
         test6: 1,
         test4: 1,
         test2: 1
       }
-    };
+    });
+  });
 
-    test(myPackageJson, patch, toPackageJson, expected);
+  it('works at the end', function() {
+    test({
+      myPackageJson: {
+        test1: 1,
+        test2: 1,
+        test6: 1
+      },
+      patch: [
+        { op: 'add', path: '/test4', value: 1 }
+      ],
+      toPackageJson: {
+        test1: 1,
+        test2: 1,
+        test3: 1,
+        test4: 1,
+        test5: 1
+      },
+      expected: {
+        test1: 1,
+        test2: 1,
+        test6: 1,
+        test4: 1
+      }
+    });
+  });
+
+  it('handles multiple patch', function() {
+    test({
+      myPackageJson: {
+        test1: 1,
+        test6: 1,
+        test2: 1,
+        test10: 1,
+        test0: 1
+      },
+      patch: [
+        { op: 'add', path: '/test4', value: 1 },
+        { op: 'add', path: '/test8', value: 1 }
+      ],
+      toPackageJson: {
+        test1: 1,
+        test3: 1,
+        test4: 1,
+        test5: 1,
+        test2: 1,
+        test7: 1,
+        test8: 1,
+        test9: 1,
+        test0: 1
+      },
+      expected: {
+        test1: 1,
+        test6: 1,
+        test4: 1,
+        test2: 1,
+        test10: 1,
+        test8: 1,
+        test0: 1
+      }
+    });
+  });
+
+  it('handles nested objects', function() {
+    test({
+      myPackageJson: {
+        test: {
+          test1: 1,
+          test6: 1,
+          test2: 1
+        }
+      },
+      patch: [
+        { op: 'add', path: '/test/test4', value: 1 }
+      ],
+      toPackageJson: {
+        test: {
+          test1: 1,
+          test3: 1,
+          test4: 1,
+          test5: 1,
+          test2: 1
+        }
+      },
+      expected: {
+        test: {
+          test1: 1,
+          test6: 1,
+          test4: 1,
+          test2: 1
+        }
+      }
+    });
   });
 
   it('performs upstream options as well', function() {
-    let myPackageJson = {
-      test1: 1,
-      test2: [1]
-    };
-    let patch = [
-      { op: 'add', path: '/test2/-', value: 2 },
-      { op: 'remove', path: '/test1' }
-    ];
-    let toPackageJson = {
-      test2: [1, 2],
-      ignored: 1
-    };
-    let expected = {
-      test2: [1, 2]
-    };
-
-    test(myPackageJson, patch, toPackageJson, expected);
+    test({
+      myPackageJson: {
+        test1: 1,
+        test2: [1]
+      },
+      patch: [
+        { op: 'add', path: '/test2/-', value: 2 },
+        { op: 'remove', path: '/test1' }
+      ],
+      toPackageJson: {
+        test2: [1, 2],
+        ignored: 1
+      },
+      expected: {
+        test2: [1, 2]
+      }
+    });
   });
 });
