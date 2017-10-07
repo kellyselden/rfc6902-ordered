e033e2186ba45026a6b558a717f9220290d83637
Kelly Selden <kellyselden@gmail.com>
2017-10-07 16:17:42 -0700

    extract add-to-object-at-index

--- src/index.js
+++ src/index.js
@@ -1,37 +1,11 @@
 'use strict';
 
 const rfc6902 = require('rfc6902');
+const addToObjectAtIndex = require('./add-to-object-at-index');
 
 const _applyPatch = rfc6902.applyPatch;
 
 module.exports = rfc6902;
-
-function addToObjectAtIndex(obj, key, value, index) {
-  let result = {};
-  let keys = Object.keys(obj);
-
-  for (let i = 0; i < keys.length; i++) {
-    if (i === index) {
-      result[key] = value;
-    }
-
-    let k = keys[i];
-    result[k] = obj[k];
-  }
-
-  if (index === keys.length) {
-    result[key] = value;
-  }
-
-  // mutate the original object because it we like that
-  // it is already linked somewhere in the package.json tree
-  for (let key in obj) {
-    delete obj[key];
-  }
-  for (let key in result) {
-    obj[key] = result[key];
-  }
-}
 
 function get(obj, parts) {
   return parts.reduce((total, next) => {
